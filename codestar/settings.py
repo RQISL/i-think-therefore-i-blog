37370fcac6d0c3da19bc8da5c0e2ab0ac9ffe295
RQISL <creativewb50@gmail.com>
2023-07-06 19:07:58 +0000

    change correct set False

--- codestar/settings.py
+++ codestar/settings.py
@@ -29,8 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = Falseee
-
+DEBUG = False
 ALLOWED_HOSTS = ['https://i-thin-i-blog-885167d863c8.herokuapp.com/',
                  '8000-rqisl-i-think-therefore-65r8mxv735.us2.codeanyapp.com',
                  'localhost']
